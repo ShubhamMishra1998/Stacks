ce10544a523cc72cb5dc0933aa4ed477cf359e74
codeking456 <sm544735@gmail.com>
2018-08-19 20:18:03 +0530

    Create Maximum Depth of nested paranthesis.cpp

---
+++ Maximum Depth of nested paranthesis.cpp
@@ -0,0 +1,38 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long int ll;
+ll maxDepth(string s1)
+{
+	ll m = 0, c = 0;
+	for (ll i = 0; i < s1.size(); i++)
+	{
+		if (s1[i] == '(')
+		{
+			c++;
+			if (c > m)
+				m = c;
+		}
+		else
+		{
+			if (c > 0)
+				c--;
+			else
+				return -1;
+		}
+	}
+	if (c != 0)
+		return -1;
+	else
+		return m;
+}
+int main()
+{
+	string s1;
+	ll a;
+	cin >> s1;
+	a = maxDepth(s1);
+	if (a != -1)
+		cout << a << ' ';
+	else
+		cout << "NOT BALANCED";
+}
